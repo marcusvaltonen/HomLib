75afdb1606f81bb6e4568da0f1edb6319e73ffc7
Marcus Valtonen Örnhag <marcusvaltonen@gmail.com>
2020-10-20 12:05:31 +0200

    remove unused variable from bash script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 mkdir -p build && cd build
 
 # Configure
-cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug ..
 
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . --config Debug -- -j $(nproc)
